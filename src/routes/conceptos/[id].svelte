1e3e09aa531bb3b9ab646efaf595eab9dbef075e
Pau Fernández <pau.fernandez@upc.edu>
2019-04-28 00:28:22 +0200

    Puliendo estilos

--- src/routes/conceptos/[id].svelte
+++ src/routes/conceptos/[id].svelte
@@ -1,15 +1,15 @@
 <script context="module">
 	export async function preload({ params, query }) {
-		// the `id` parameter is available because
-		// this file is called [id].svelte
-		const res = await this.fetch(`conceptos/${params.id}.json`);
-		const data = await res.json();
+	  // the `id` parameter is available because
+	  // this file is called [id].svelte
+	  const res = await this.fetch(`conceptos/${params.id}.json`);
+	  const data = await res.json();
 
-		if (res.status === 200) {
-			return { node: data };
-		} else {
-			this.error(res.status, data.message);
-		}
+	  if (res.status === 200) {
+	    return { node: data };
+	  } else {
+	    this.error(res.status, data.message);
+	  }
 	}
 </script>
 
@@ -17,18 +17,49 @@
 	export let node;
 </script>
 
+<style>
+  .header {
+    margin-bottom: 1em;
+  }
+  .header h1 {
+    margin-bottom: 0;
+  }
+  .header ul {
+    padding: 0;
+    margin: 0;
+    min-height: 1.5em;
+  }
+  .header li {
+    display: inline-block;
+    padding: 0;
+    margin-right: 0.7em;
+  }
+  .header li a {
+    font-size: 85%;
+    color: gray;
+    text-decoration: none;
+  }
+</style>
+
 <svelte:head>
 	<title>{node.title}</title>
 </svelte:head>
 
-<h1>{node.title}</h1>
+<div class="header">
+  <ul>
+  {#each node.parents as parent}
+      <li><a href={'conceptos/' + parent.id}>{parent.title}</a></li>
+  {/each}
+  </ul>
+  <h1>{node.title}</h1>
+  <ul>
+    {#each node.bases as base}
+    <li><a href={'conceptos/' + base.id}>{base.title}</a></li>
+    {/each}
+  </ul>
+</div>
 
 <h2>Bases:</h2>
-<ul>
-{#each node.bases as base}
-<li><a href={'conceptos/' + base.id}>{base.title}</a></li>
-{/each}
-</ul>
 
 <h2>Derivados:</h2>
 <ul>
@@ -44,10 +75,5 @@
 {/each}
 </ul>
 
-<h2>Contexto:</h2>
-<ul>
-{#each node.parents as parent}
-<li><a href={'conceptos/' + parent.id}>{parent.title}</a></li>
-{/each}
-</ul>
+
 
