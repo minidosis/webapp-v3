37276bc91a8318cfb2d9e48ae648e9e6787d8f2d
Pau Fernández <pau.fernandez@upc.edu>
2019-04-28 19:18:39 +0200

    Cabecera colapsable

--- src/routes/conceptos/[id].svelte
+++ src/routes/conceptos/[id].svelte
@@ -6,7 +6,7 @@
 	  const data = await res.json();
 
 	  if (res.status === 200) {
-	    return { node: data };
+	    return { node: data, expanded: false };
 	  } else {
 	    this.error(res.status, data.message);
 	  }
@@ -15,8 +15,59 @@
 
 <script>
   import markright from '../../markright';
-	export let node;
+  export let node;
+  let expanded = false;
+
+  const toggleExpanded = () => { expanded = !expanded }
 </script>
+
+<svelte:head>
+	<title>{node.title}</title>
+</svelte:head>
+
+<div class="header">
+  <h1>{node.title} <button on:click={toggleExpanded}>{expanded ? 'hide' : 'show'}</button> </h1>
+</div>
+
+{#if expanded}
+  <h2>Padres</h2>
+  <ul>
+    {#each node.parents as parent}
+        <li><a href={'conceptos/' + parent.id}>{parent.title}</a></li>
+    {/each}
+  </ul>
+
+  <h2>Bases</h2>
+  <ul>
+    {#each node.bases as base}
+    <li><a href={'conceptos/' + base.id}>{base.title}</a></li>
+    {/each}
+  </ul>
+
+  <h2>Derivados</h2>
+  <ul>
+    {#each node.derived as deriv}
+    <li><a href={'conceptos/' + deriv.id}>{deriv.title}</a></li>
+    {/each}
+  </ul>
+
+  <h2>Partes</h2>
+  <ul>
+    {#each node.children as child}
+    <li><a href={'conceptos/' + child.id}>{child.title}</a></li>
+    {/each}
+  </ul>
+{/if}
+
+<div class="content">
+{@html markright.genHtml(node.content, {
+  minidosis: (args, children) => {
+    return `<a href=${'conceptos/' + args[0]}>${markright.genHtml(children)}</a>`;
+  }
+})}
+</div>
+
+
 
 <style>
   .header {
@@ -25,62 +76,4 @@
   .header h1 {
     margin-bottom: 0;
   }
-  .header ul {
-    padding: 0;
-    margin: 0;
-    min-height: 1.5em;
-  }
-  .header li {
-    display: inline-block;
-    padding: 0;
-    margin-right: 0.7em;
-  }
-  .header li a {
-    font-size: 85%;
-    color: gray;
-    text-decoration: none;
-  }
 </style>
-
-<svelte:head>
-	<title>{node.title}</title>
-</svelte:head>
-
-<div class="header">
-  <ul>
-  {#each node.parents as parent}
-      <li><a href={'conceptos/' + parent.id}>{parent.title}</a></li>
-  {/each}
-  </ul>
-  <h1>{node.title}</h1>
-  <ul>
-    {#each node.bases as base}
-    <li><a href={'conceptos/' + base.id}>{base.title}</a></li>
-    {/each}
-  </ul>
-</div>
-
-<h2>Bases:</h2>
-
-<h2>Derivados:</h2>
-<ul>
-{#each node.derived as deriv}
-<li><a href={'conceptos/' + deriv.id}>{deriv.title}</a></li>
-{/each}
-</ul>
-
-<h2>Partes:</h2>
-<ul>
-{#each node.children as child}
-<li><a href={'conceptos/' + child.id}>{child.title}</a></li>
-{/each}
-</ul>
-
-<div class="content">
-{@html markright.genHtml(node.content, {
-  md: (args, children) => {
-    return `<a href=${'conceptos/' + args[0]}>${markright.genHtml(children)}</a>`;
-  }
-})}
-</div>
-
