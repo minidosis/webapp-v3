249afd02e012435a50f0645542db8b30b758ea32
Pau Fernández <pau.fernandez@upc.edu>
2019-12-15 15:28:04 +0100

    Adapto genhtml a los nuevos métodos de comando

--- src/genhtml.js
+++ src/genhtml.js
@@ -15,21 +15,24 @@ const escape = (text) => {
 
 class HtmlFuncMap {
 
-  minidosis(args, children) { return `<a href="${"id/" + args[0]}">${parse(children, this)}</a>` }
-  a(args, children) { return `<a href="${args[0]}">${parse(children, this)}</a>` }
-  b(_, children) { return `<b>${parse(children, this)}</b>` }
-  em(_, children) { return `<em>${parse(children, this)}</em>` }
-  h2(_, children) { return `<h2>${parse(children, this)}</h2>` }
+  minidosis({ args, rawChildren }) {
+    return `<a href="${"id/" + args[0]}">${parse(rawChildren, this)}</a>`
+  }
 
-  olist(_, children) {
+  a({ args, rawChildren }) { return `<a href="${args[0]}">${parse(rawChildren, this)}</a>` }
+  b({ rawChildren }) { return `<b>${parse(rawChildren, this)}</b>` }
+  em({ rawChildren }) { return `<em>${parse(rawChildren, this)}</em>` }
+  h2({ rawChildren }) { return `<h2>${parse(rawChildren, this)}</h2>` }
+
+  olist({ rawChildren }) {
     let html = `<div class="enumerate">`
     let funcMap = this
     let num = 1
-    parse(children, {
-      row(_, children) {
+    parse(rawChildren, {
+      row({ rawChildren }) {
         html += `<div class="item"><span class="num">${num}</span>`
         html += `<div class="content">`
-        html += parse(children, funcMap)
+        html += parse(rawChildren, funcMap)
         html += `</div></div>`
         num++
       },
@@ -39,14 +42,14 @@ class HtmlFuncMap {
     return html
   }
 
-  ulist(_, children) {
+  ulist({ rawChildren }) {
     let html = `<div class="itemize">`
     let funcMap = this
-    parse(children, {
-      row(_, children) {
+    parse(rawChildren, {
+      row({ rawChildren }) {
         html += `<div class="item"><span class="bullet">&bull;</span>`
         html += `<div class="content">`
-        html += parse(children, funcMap)
+        html += parse(rawChildren, funcMap)
         html += `</div></div>`
       },
     })
@@ -54,52 +57,52 @@ class HtmlFuncMap {
     return html
   }
 
-  pre(args, children) {
+  pre({ args, rawChildren }) {
     const [lang, _class] = args ? args : [];
     let html = ''
     html += `<div class="pre ${_class ? _class : ""}">`
     html += `<pre><code class="language-${lang}">`
-    html += parse(children, {
+    html += parse(rawChildren, {
       b: this.b,
       box: this.box,
-      __block__(children) { return children ? children.join('\n') : '' },
-      __line__(children) { return children ? children.join('') : '' },
-      __text__(text) { return escape(text) },
+      __text__: ({ text }) => escape(text),
+      __block__: ({ children }) => (children ? children.join('\n') : ''),
+      __line__: ({ children }) => (children ? children.join('') : ''),
     })
     html += `</code></pre></div>`
     return html
   }
 
-  code(_, children) {
-    return `<span class="code">${escape(parse(children, this))}</span>`
+  code({ rawChildren }) {
+    return `<span class="code">${escape(parse(rawChildren, this))}</span>`
   }
 
-  img(_, children) { return `<img src="asset/${children[0]}" />` }
+  img({ rawChildren }) { return `<img src="asset/${rawChildren[0]}" />` }
 
-  box(_, children) {
-    return `<span class="box">${parse(children, this)}</span>`
+  box({ rawChildren }) {
+    return `<span class="box">${parse(rawChildren, this)}</span>`
   }
 
-  header(_, children) {
+  header({ rawChildren }) {
     let html = `<thead><tr>`
-    console.log("header", children)
-    children.forEach(ch => {
+    console.log("header", rawChildren)
+    rawChildren.forEach(ch => {
       html += `<th>${ch.children[0]}</th>`
     })
     html += `</tr></thead>`
     return html
   }
 
-  footnote(args, children) {
+  footnote({ args, rawChildren }) {
     const footnum = `<span class="footnote">${args[0]}</span>`
-    if (children) {
-      return `<div class="footnote">${footnum}${parse(children, this)}</div>`
+    if (rawChildren) {
+      return `<div class="footnote">${footnum}${parse(rawChildren, this)}</div>`
     } else {
       return footnum
     }
   }
 
-  table(args, children) {
+  table({ args, rawChildren }) {
     const funcMap = this
     let html = `<div class="table">`
     if (args && args[0] === "left") {
@@ -107,36 +110,35 @@ class HtmlFuncMap {
     } else {
       html += `<table>`
     }
-    html += parse(children, {
+    html += parse(rawChildren, {
       __text__: funcMap.__text__,
       // TODO: Aquí hay que duplicar el 'code' sin hacer parse porque no funciona bien...
-      code(_, children) {
-        return `<span class="code">${escape(children.join(''))}</span>`
+      code({ rawChildren }) {
+        return `<span class="code">${escape(rawChildren)}</span>`
       },
-      __block__(children) {
+      __block__({ children }) {
         return children.map(row => `<tr>${row}</tr>`).join('')
       },
-      header(_, children) {
-        return children[0].split('|').map(h => `<th>${h.trim()}</th>`).join('')
+      header({ rawChildren }) {
+        return rawChildren.split('|').map(h => `<th>${h.trim()}</th>`).join('')
       },
-      __line__(children) {
+      __line__({ children }) {
         return children.join('').split('|').map(td => `<td>${td}</td>`).join('')
       }
     })
     html += `</table>`
     html += `</div>`
-    console.log(html)
     return html
   }
 
-  __text__(text) { return text }
-  __line__(children) { return (children ? children.join('') : '') }
-  __block__(children) { return children ? children.join('\n') : '' }
+  __text__({ text }) { return text }
+  __line__({ children }) { return (children ? children.join('') : '') }
+  __block__({ children }) { return children ? children.join('\n') : '' }
 
   __command__(node) { return `<span class="error">Cmd <code>"${node.id}"</code> not found</span>` }
 }
 
-const genHtml = (str) => parse(str.split('\n'), new HtmlFuncMap())
+const genHtml = (str) => parse(str, new HtmlFuncMap())
 
 module.exports = {
   genHtml
